7d89891e9d45798d2c9ac04c4195b6238e761894
RuthGare <163552738+RuthGare@users.noreply.github.com>
2024-12-02 11:11:51 +0300

    nodemon install

--- Backend/server.js
+++ Backend/server.js
@@ -39,32 +39,3 @@
 // app.listen(PORT, () => {
 //   console.log(`Server is running on http://localhost:${PORT}`);
 // });
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const pool = require("./db");
-
-//middleware
-app.use(cors());
-app.use(express.json());
-
-//routes
-
-//create user
-app.post("/users",async(req, res)=>{
-  try {
-
-   console.log(req.body);
-
-  } catch(err){
-
-  console.error(err.message);
-  
-}})
-//get all user
-//get a user
-//update user
-//delete user
-app.listen(5000, ()=>{
-  console.log("server has started on port 5000")
-})
